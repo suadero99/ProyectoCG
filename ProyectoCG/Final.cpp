59b6d86e82e171eca8441f2ac9b02b237cab97df
suadero99 <froysantos99@gmail.com>
2022-05-08 19:09:53 -0500

    Añadidos edificios al código

--- ProyectoCG/Final.cpp
+++ ProyectoCG/Final.cpp
@@ -402,6 +402,8 @@ int main()
 	Model miCubo("resources/objects/MiCubo/cubo.obj"); //Cubo agregado*/
 	Model piso("resources/objects/piso/piso.obj");
 	Model arbol("resources/objects/fauna/green_tree.obj");
+	Model qfront("resources/objects/qfront/q_front.obj");
+	Model magnet("resources/objects/magnet/magnet.obj");
 
 	//Inicialización de KeyFrames
 	for (int i = 0; i < MAX_FRAMES; i++)
@@ -618,9 +620,24 @@ int main()
 		piso16.Draw(staticShader);
 		*/
 		//Dibujo de árbol de prueba
+		/*model = glm::mat4(1.0f);
 		model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
 		staticShader.setMat4("model", model);
-		arbol.Draw(staticShader);
+		arbol.Draw(staticShader);*/
+
+		//Dibujo edificio QFront
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(-100.0f, 0.0f, 80.0f));
+		model = glm::rotate(model, glm::radians(135.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		staticShader.setMat4("model", model);
+		qfront.Draw(staticShader);
+
+		//Dibujo edificio Magnet
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(-60.0f, 0.0f, 0.0f));
+		model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		staticShader.setMat4("model", model);
+		magnet.Draw(staticShader);
 
 		/*model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -70.0f));
 		model = glm::scale(model, glm::vec3(5.0f));
